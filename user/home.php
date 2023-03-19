ee25905c8565e66e7af8d787f4a66fa3937da7f5
RIF-creativity <126873296+RIF-creativity@users.noreply.github.com>
2023-03-19 16:23:26 +0700

    countdown timer

---
+++ user/home.php
@@ -0,0 +1,86 @@
+<?php
+require("function.php");
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/header.css">
+  <link rel="stylesheet" href="../countdown-flip-timer/styles.css">
+  <script src="../countdown-flip-timer/script.js"></script>
+  <title>Document</title>
+</head>
+
+<body>
+<div class="timer">
+  <h1>MAULID 2030 <br> <span>coming soon</span></h1>
+  <div class="container">
+    <div class="container-segment">
+      <div class="segment-title">Hours</div>
+      <div class="segment">
+        <div class="flip-card" data-hours-tens>
+          <div class="top">2</div>
+          <div class="bottom">2</div>
+        </div>
+        <div class="flip-card" data-hours-ones>
+          <div class="top">4</div>
+          <div class="bottom">4</div>
+        </div>
+      </div>
+    </div>
+    <div class="container-segment">
+      <div class="segment-title">Minutes</div>
+      <div class="segment">
+        <div class="flip-card" data-minutes-tens>
+          <div class="top">0</div>
+          <div class="bottom">0</div>
+        </div>
+        <div class="flip-card" data-minutes-ones>
+          <div class="top">0</div>
+          <div class="bottom">0</div>
+        </div>
+      </div>
+    </div>
+    <div class="container-segment">
+      <div class="segment-title">Seconds</div>
+      <div class="segment">
+        <div class="flip-card" data-seconds-tens>
+          <div class="top">0</div>
+          <div class="bottom">0</div>
+        </div>
+        <div class="flip-card" data-seconds-ones>
+          <div class="top">0</div>
+          <div class="bottom">0</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <div id="navbar">
+    <a class="active" href="home.html">Home</a>
+    <a href="lomba.php">lomba</a>
+    <a href="">jadwal</a>
+    <a href="pendaftaran.php">pendaftaran</a>
+    <a href="https://api.whatsapp.com/send?phone=6283896059029">kontak</a>
+  </div>
+
+
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quaerat ratione sit quisquam dicta ut sequi soluta mollitia. In dolorem fuga hic omnis, nesciunt est fugit minus eaque reprehenderit perspiciatis.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate delectus illum sequi quas eius voluptates accusantium, voluptatem tempora perspiciatis. Provident, impedit non? Laboriosam laborum corrupti maxime, corporis et velit voluptate.
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum qui earum hic eveniet aliquam blanditiis dolorum porro temporibus labore impedit explicabo obcaecati delectus aliquid, at enim, aspernatur alias ullam amet?
+  </div>
+
+  <script src="../js/index.js"></script>
+</body>
+
+</html>
